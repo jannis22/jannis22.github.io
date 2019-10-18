bce57cdfa0117729ada79764ced000ca2f5f0d63
jannis22 <56292180+jannis22@users.noreply.github.com>
2019-10-18 10:45:31 +0200

    Update login.inc.php

--- php/login.inc.php
+++ php/login.inc.php
@@ -4,7 +4,7 @@ session_start();
 
 if(isset($_POST['n']) && $_POST['n'] != "") {
     $_SESSION['user'] = $_POST['n'];
-    header("Location: seite_2.php");
+    header("Location: menue.php");
     exit;
 }
 
